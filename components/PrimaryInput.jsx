d06286e254c4da0d7a0e284a8ed33cd1d94580c9
AyobamiMasterpiece <ayotibami14@gmail.com>
2025-01-02 07:29:32 +0100

    fixedinput

--- components/PrimaryInput.jsx
+++ components/PrimaryInput.jsx
@@ -24,7 +24,7 @@ export default function PrimaryInput(props) {
         style={{
           fontSize: 12,
           borderRadius: 12,
-          backgroundColor: "pink",
+
           borderColor: Colors.secondaryBlue,
           paddingVertical: 10,
           paddingHorizontal: 5,
