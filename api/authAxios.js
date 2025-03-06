93de649aec15bfbc6261fcf328a1c4c2fbbb8d0a
AyobamiMasterpiece <ayotibami14@gmail.com>
2025-03-06 06:46:56 +0100

    done

--- api/authAxios.js
+++ api/authAxios.js
@@ -6,7 +6,7 @@ import { AuthContext } from "../context/AuthContext";
 // Create an instance of axios with default settings
 const authAxios = axios.create({
   // baseURL: 'http://127.0.0.1:8000/',
-  baseURL: "https://easypayvtu.onrender.com/",
+  baseURL: "https://easypayvtu-9n74.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
--- api/axios.js
+++ api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://easypayvtu.onrender.com/",
+  baseURL: "https://easypayvtu-9n74.onrender.com/",
   // baseURL: 'http://127.0.0.1:8000/',
   headers: {
     "Content-Type": "application/json",
